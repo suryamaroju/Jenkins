47908f9994eed63277d91c0ccc873e934cbddb01
Surya Prakash Maroju <v-sumaro@microsoft.com>
2019-09-17 04:08:21 +0530

    Edditted the hello.java

--- java-maven-junit-helloworld-master/src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ java-maven-junit-helloworld-master/src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -3,7 +3,7 @@ package com.example.javamavenjunithelloworld;
 import java.io.PrintStream;
 
 /**
- * Simple class that says "Hello!".
+ * Simple class that says "Hello!". this is also surya
  */
 public class Hello {
 
